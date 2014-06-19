4387fc547c50cc56fd59c94bf41062f3622f4811
Haoyuan Li <haoyuan@cs.berkeley.edu>
2014-06-18 19:31:36 -0700

    compile fix

--- main/src/main/java/tachyon/worker/TachyonWorker.java
+++ main/src/main/java/tachyon/worker/TachyonWorker.java
@@ -16,6 +16,7 @@ package tachyon.worker;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.net.UnknownHostException;
 
 import org.apache.log4j.Logger;
 import org.apache.thrift.TException;
